65d21a21d9099e970cd0fd1e2aaef38eee0a7248
thejayadad <thejayadad@gmail.com>
2023-09-28 07:01:22 -0400

    update

---
+++ myapp/app/account/bookings/page.js
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+const Bookings = () => {
+  return (
+    <secttion>
+        <h2>Bookings</h2>
+    </secttion>
+  )
+}
+
+export default Bookings
---
+++ myapp/app/account/ride/page.js
@@ -0,0 +1,21 @@
+'use client'
+
+import Link from 'next/link'
+import React from 'react'
+import { FaCar } from "react-icons/fa";
+
+const Ride = () => {
+  return (
+    <section className='px-4 flex flex-col'>
+    <div className="flex">
+      <Link href="/account/new">
+        <span className="flex items-center px-4 py-2 text-white bg-purple-400 rounded-sm hover:bg-purple-600 transition duration-300 ease-in-out">
+          <FaCar className="mr-2" /> Add Ride
+        </span>
+      </Link>
+    </div>
+    </section>
+  )
+}
+
+export default Ride
